fd805c1afcd2edd962c8c65816e030d024517e39
YenPeiChen07 <jenny35006@gmail.com>
2021-08-03 20:59:30 +0800

    modify layout

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> style="margin:0!important; >
     <head>
     <meta charset="<?php bloginfo('charset');?>" />
     <title><?php
@@ -17,15 +17,27 @@
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <link href="<?php bloginfo('template_directory') ?>/style.css" media="screen" rel="stylesheet" type="text/css" />
     </head>
+    <?php
+        if(is_page('homepage')){ /*首頁*/
+            echo '<link rel="stylesheet" href="';bloginfo('template_url');
+            echo '/css/homepage.css" type="text/css" media="screen and (min-width: 701px)" />';
+            /*************/
+            echo '<script src="';bloginfo('template_url');
+            echo '/js/homepage.js" type="text/javascript" media="screen and (min-width: 701px)"></script>';
+            /*************/
+            echo '<link rel="stylesheet" href="';bloginfo('template_url');
+            echo'/mobile-css/homepage-mobile.css" type="text/css" media="screen and (max-width: 700px)" />';
+        }
+
+    ?>
     <body>
     <div class="container">
         <header class="header">
         <div class="title">
             <div class="logo"></div>
             <a class="web_name" href="<?php bloginfo('url'); ?>">
-                <div>國立陽明交通大學<br>公共衛生研究所<br><span class="enName">Institute of Public Health, NYCU</span></div>
+                <span class="Name">國立陽明交通大學<br>公共衛生研究所<br></span>
+                <span class="enName">Institute of Public Health, NYCU</span>
             </a>
         </div>
-
-        <div class="slidershow"></div>
         </header>
---
+++ page-templates/template-homepage.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Template Name: homepage
+ */
+?>
+
+
+<?php get_header(); ?>
+<!--<script type="text/javascript" src="<?php// bloginfo('template_url')?>/js/homepage.js"></script>-->
+<head> <link href="css/homepage.css" rel="stylesheet" type="text/css"> </head>
+
+<div class="homepage">
+    
+</div>
