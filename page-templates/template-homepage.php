7d2909d18170e584509d8c79bc73b41b289ba959
YenPeiChen07 <jenny35006@gmail.com>
2021-08-05 15:49:36 +0800

    add hp-news part

--- page-templates/template-homepage.php
+++ page-templates/template-homepage.php
@@ -12,6 +12,9 @@
 <div class="homepage">
     <div class="slidershow-container">    </div>
 
+
+
+
    <!-- the News -->
    <div class="News-container">
       <div class="news-title">
@@ -19,31 +22,51 @@
             <span class="ch-title">最新消息<br></span>
             <span class="en-title">News</span>
       </div>
-      <?php
+      <?php //query the recent 6 posts
             $args = array(
             'post_type' => 'post',
             'post_status' => 'publish',
             'category_name' => 'news',
-            'order'=>'DESC');
+            'orderby'=>'date',
+            'posts_per_page' => 6);
             $arr_posts = new WP_Query( $args );
 
             if ( $arr_posts->have_posts() ) :
       ?>
       <div class="news-article">
-            <?php while ( $arr_posts->have_posts() ) : $arr_posts->the_post(); ?>
-                  <div class="article-content">
-                  <div class="article-meta">
-                        <span class="post_time"><?php the_time('Y.m.j'); ?></span>
-                        <span class="post_category"><?php the_category(' , '); ?></span>
-                        <!--<span><?php //the_tags('', ' , ', ''); ?></span>-->
-                  </div>
-                  <div class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
-                  <!--<div class="excerpt'></div>-->
-                  <?php the_excerpt(); ?>
-                  <div class="clearfix"></div>
+            <?php             
+                  $counter=0;
+                  while ( $arr_posts->have_posts() ) : 
+                        $arr_posts->the_post(); 
+                        $counter = $counter + 1;
+            ?>
+                  <div class="article-content num-<?php echo $counter ?>">
+                        <div class="post_counter <?php echo $counter ?>"><?php echo "0" . $counter . "."?>&nbsp;&nbsp;</div>
+                        <img class="thumbnail_icon" src="<?php bloginfo('template_url')?>/images/icon/icon-newspaper50.svg">
+                        <div class="article-meta">
+                              <img class="icon-clock>" src="<?php bloginfo('template_url')?>/images/icon/icon-clock.svg">
+                              <span class="post_time"><?php the_time('Y.m.j'); ?></span>
+                              <span class="post_category"><?php the_category(' , '); ?></span>
+                              <!--<span><?php //the_tags('', ' , ', ''); ?></span>-->
+                        </div>
+                        <div class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+                        <div class="excerpt <?php echo $counter ?>"> <?php the_excerpt(); ?> </div>
+                        <div class="clearfix"></div>
                   </div>
             <?php endwhile; ?>
       </div>
-      <?php endif; wp_reset_postdata();?>
+      <?php endif; wp_reset_postdata(); ?> 
+      <!-- js for hover event-->
+      <script>
+            function showUP(x) {
+            x.style.height = "64px";
+            x.style.width = "64px";
+            }
+
+            function hidden(x) {
+            x.style.height = "32px";
+            x.style.width = "32px";
+            }
+      </script>
    </div>
 </div>
