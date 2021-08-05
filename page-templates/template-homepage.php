4ae8053c4b120af02be9089a7b97039aee3de483
YenPeiChen07 <jenny35006@gmail.com>
2021-08-05 10:45:52 +0800

    add the icon and title

--- page-templates/template-homepage.php
+++ page-templates/template-homepage.php
@@ -10,16 +10,16 @@
 <head> <link href="css/homepage.css" rel="stylesheet" type="text/css"> </head>
 
 <div class="homepage">
-    <div class="slidershow-container">
-    </div>
+    <div class="slidershow-container">    </div>
 
    <!-- the News -->
    <div class="News-container">
-   <div class="news-icon">
-         <div class="icon"></div>
-         <span class="title">最新消息</span>
-   </div>
-   <?php
+      <div class="news-title">
+            <div class="icon"></div>
+            <span class="ch-title">最新消息<br></span>
+            <span class="en-title">News</span>
+      </div>
+      <?php
             $args = array(
             'post_type' => 'post',
             'post_status' => 'publish',
@@ -28,24 +28,22 @@
             $arr_posts = new WP_Query( $args );
 
             if ( $arr_posts->have_posts() ) :
-   ?>
-   <?php while ( $arr_posts->have_posts() ) : $arr_posts->the_post(); ?>
-      <div class="article-content">
-         <div class="article-meta">
-               <span class="post_time"><?php the_time('Y.m.j'); ?></span>
-               <span class="post_category"><?php the_category(' , '); ?></span>
-               <!--<span><?php //the_tags('', ' , ', ''); ?></span>-->
-         </div>
-         <div class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
-         <!--<div class="excerpt'></div>-->
-         <?php the_excerpt(); ?>
-         <div class="clearfix"></div>
+      ?>
+      <div class="news-article">
+            <?php while ( $arr_posts->have_posts() ) : $arr_posts->the_post(); ?>
+                  <div class="article-content">
+                  <div class="article-meta">
+                        <span class="post_time"><?php the_time('Y.m.j'); ?></span>
+                        <span class="post_category"><?php the_category(' , '); ?></span>
+                        <!--<span><?php //the_tags('', ' , ', ''); ?></span>-->
+                  </div>
+                  <div class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+                  <!--<div class="excerpt'></div>-->
+                  <?php the_excerpt(); ?>
+                  <div class="clearfix"></div>
+                  </div>
+            <?php endwhile; ?>
       </div>
-   <?php endwhile; ?>
-
-   <?php endif;
-         wp_reset_postdata();?>
+      <?php endif; wp_reset_postdata();?>
    </div>
-
-
 </div>
