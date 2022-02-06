640e12aaa20e4b57f355ef7a4aa140e86101a5f5
YenPeiChen07 <jenny35006@gmail.com>
2022-02-06 22:31:13 +0800

    fix animation

--- js/draggable-slides.js
+++ js/draggable-slides.js
@@ -2,15 +2,13 @@ const container = document.querySelector(".Interview-container");
 const cards = document.querySelector(".cards");
 
 let cards_margin = 0;
+let isAnimate = false;
+let movelen = 0;
 function animate(){
-  const container_rect = container.getBoundingClientRect();
-  const cards_rect = cards.getBoundingClientRect();
-  var margin = cards_margin;
   int = setInterval(function() {
-    const Xextra = cards_rect.right - container_rect.right;
-    margin = (margin > Xextra ?  Xextra : margin + 1);
-    cards_margin = margin;
-    cards.style.marginLeft = -margin + "px";
+    cards_margin++;
+    cards.style.left = -cards_margin + "px";
+    boundCards();
   },
   1000 / 15)
 }
@@ -18,12 +16,14 @@ function animate(){
 animate();
 
 container.addEventListener("mouseleave", () =>{
+  isAnimate = true;
   animate();
 });
 
 container.addEventListener("mouseover", () =>{
+  isAnimate = false;
   clearInterval(int);
-  cards.style.marginLeft = -cards_margin + "px";
+  //cards.style.left = -cards_margin + "px";
 });
 
 /* keep track of user's mouse down and up */
@@ -33,38 +33,44 @@ let cursorXSpace;
 
 container.addEventListener("mousedown", (e) => {
   /*the 'e' means event*/
+  /*按下 mouse*/
   isPressedDown = true;
   cursorXSpace = e.offsetX - cards.offsetLeft; 
   container.style.cursor = "grabbing";
 });
 
 container.addEventListener("mouseup", () => {
+  /*放開 mouse*/
   container.style.cursor = "grab";
+  
 });
 
 window.addEventListener("mouseup", () => {
   isPressedDown = false;
 });
 
-
 container.addEventListener("mousemove", (e) => {
   if (!isPressedDown) return;
   e.preventDefault();
   cards.style.left = `${e.offsetX - cursorXSpace}px`;
+  cards_margin = -(e.offsetX - cursorXSpace);
   boundCards();
 });
 
 function boundCards() {
   const container_rect = container.getBoundingClientRect();
   const cards_rect = cards.getBoundingClientRect();
-
-  if (parseInt(cards.style.left) > 0) {
+  
+  if (parseInt(cards.style.left) > 0){
     cards.style.left = 0;
-  } else if (cards_rect.right < container_rect.right) {
+    cards_margin = 0;
+  } 
+  else if (cards_rect.right < container_rect.right) {
+    if(isAnimate) {
+      isAnimate = false;
+      clearInterval(int);
+    }
     cards.style.left = `-${cards_rect.width - container_rect.width}px`;
+    cards_margin = cards_rect.width - container_rect.width;
   }
 }
-
-
-
-//cards.addEvenetListener("mouseenter",animation_reset);
