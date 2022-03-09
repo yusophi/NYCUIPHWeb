6a1ce08667bfcc52344c6dc62a022691c1f9305e
milokuo <abber.kuo.2001@gmail.com>
2022-03-09 15:13:04 +0800

    完成領域課程與校友的話

--- header.php
+++ header.php
@@ -161,7 +161,11 @@
             echo '/css/singlepost.css" type="text/css" media="screen and (min-width: 701px)" />';
             /*************/
          }
-
+         if(is_page_template( 'page-templates/template-epid.php' )){ /*流行病學模板*/
+            echo '<link rel="stylesheet" href="';bloginfo('template_url');
+            echo '/css/epid.css" type="text/css" media="screen and (min-width: 701px)" />';
+            /*************/
+         }
     ?>
     </head>
     <script type="text/javascript" src="<?php bloginfo('template_url')?>/js/header.js"></script>
--- page-templates/template-epid.php
+++ page-templates/template-epid.php
@@ -1,1 +1,181 @@
-<h1>helllo</h1>
+<?php
+/*
+ * Template Name: epid
+ */
+?>
+
+<?php get_header(); ?>
+
+<div class="epid_page">
+    <div class="upper_instruction">
+        <div class="black_circle1 black_circle"></div>
+        <div class="black_circle2 black_circle"></div>
+        <div id="title">
+            <div id="ch_title">流行病學領域</div>
+            <div id="en_title">Division of Epidemiology</div>
+        </div>
+        <div id="instruction">
+            <div class="instru_img_container"><img id="instru_img" src="<?php bloginfo('template_url') ?>/images/page_epid/key_vision.png"></div>
+            <div id="instru_content">流行病學主要是調查人群中疾病、殘障與死亡的分佈及其決定因素，透過系統性的觀察來瞭解生理現象、致病機轉、疾病的自然史及病因探討。教學的目標在於培育具有社區醫學整體觀念，運用生物醫學統計、流行病學、疾病防治、衛生教育及衛生政策與管理等相關理論知識於研究設計及實務分析，將理論研究帶入實務領域， 並進行社區健康問題的診斷與疾病之偵測，進而研擬並推動預防與保健之介入計畫。
+                <br><br>
+                本組教學內容的設計在於奠定流行病學、生物統計與社區醫學研究方法之基礎，並加強生物醫學之基本知識，著重個別興趣的發展與分析整合之能力，並提供社區醫學相關研究環境，讓學生能直接接觸資料收集的實務工作，運用生物統計、流行病學、疾病防治、衛生教育等的理論知識於研究設計及決策分析上，共同發展社區醫學之實證研究。
+                <br><br>
+                本組乃結合基礎醫學、臨床醫學與社區醫學的專家，進行社區疾病篩檢之大規模長期計畫，主要研究方向包括糖尿病、高血壓、冠狀動脈性心臟疾病、痛風、高尿酸血症、腎臟病、眼睛疾病、骨質疏鬆症、精神疾病與健康相關之生活品質等研究主軸，並搭配流行病學之研究設計，兼顧研究與服務，使預防醫學在各社區落實生根。
+                <br><br>
+                ※自103學年起，流行病學與預防醫學領域合併為流行病學領域。
+                <br><br>
+                原預防醫學領域相關訊息，請參照以下內容：
+                (1) 修業規定 (2) 預防醫學領域研究方向修課建議
+            </div>
+        </div>
+    </div>
+    <div id="divider"></div>
+    <div id="divider_components">
+        <div class="small_black_circle"></div>
+        <div class="down_arrow"></div>
+        <div class="section_indicator">s0</div>
+    </div>
+    <div class="epid_buttons">
+        <div id="epid_classes_btn" class="epid_btn animation1_btn">領域課程</div>
+        <div id="epid_teachers_btn" class="epid_btn animation1_btn">領域師資</div>
+        <div id="epid_thoughts_btn" class="epid_btn animation1_btn">校友的話</div>
+    </div>
+    <div id="changing_content">
+        <div class="tgh_segment">
+            <div class="tgh_title">蕭逸凡【流行病學領域碩士班畢業】</div>
+            <br>
+            <div class="tgh_passage">
+                我來自於中國醫藥大學公共衛生學系，大學畢業後因為自己對著醫學與公共衛生的發展抱持著好奇的心態，所以於大學畢業前夕便決定報考研究所，很幸運地來到陽明公衛所。到陽明報到的第一天，令我印象最深刻的是每位學生的背景，這項特色將重新定義我對公共衛生的認識，這也是陽明公衛所有別於一般研究所的性質。大學以來我一直環繞在公共衛生的領域，但不諱言的和大家分享，直到大學畢業前才對於公共衛生有基礎上的認識，認識甚麼是公共衛生、流行病學、生物統計、衛生政策等，因為公共衛生要了解的知識太多除此之外還需要有實務上的經歷才能慢慢理解公共衛生的邏輯概念。來到陽明公衛所，因為這裡的學生來自社會上四面八方的人才，所以在上課的時候常常能聽到各種不同的角度來看待同一件事情。縱然用不同的角度來討論一件事情是學習上的好處，但要怎麼融合成各位學生能理解並且認同的知識，這就需要仰賴陽明公衛所的每一位老師。
+                陽明公衛所的第二項特色即是師資。公共衛生最初的目的便是將專業的知識傳達給公眾理解，但是從專業到非專業的過程中需要一道橋梁，來連接中間的鴻溝，但是這段鴻溝的管道會有很多種形式出現，所以在國外公共衛生的教學網羅了各專業領域的師資，透過他們的專業，重新定義公共衛生的概念，再次傳達給學生了解。陽明公衛所同樣也看到相似的情境，因此在師資的選擇上不單單只針對醫學、公共衛生，更有法律、統計、運動等等專業的師資來傳達公共衛生。同時在課程的開設上，也不只針對公共衛生的知識學習，更針對不同的角度來審視公共衛生在不同領域中的實務應用。這裡的老師同樣地也受到專業且多元的訓練，所以在各種課堂的討論中，除了學生們的熱烈回應外，老師們也同樣能提出相對應的回答來統合各界的認知重新傳達公共衛生。
+                我在陽明公衛所碩士生活的兩年，回首過去這段日子，論文撰寫的過程真的很辛苦但努力過後的興奮絕對遠大於之前辛苦的過程，雖然過程艱辛但陽明公衛所就像個大家庭，上至所長、老師下至學、弟妹，大家和樂融融照應彼此。過年過節時分，這裡的研究生依然持續作業，但總不忘過節的氣氛，老師們也常常出人意表的傳達過節氛圍，這段過程將會是我整個學習旅程中最棒的回憶，若問我能有機會重新選擇繼續進修的地方，我相信我依然還是會選擇陽明公共衛生研究所！
+            </div>
+            <div class="tgh_title margin_top">
+                近五年流病組碩士班校友流向
+            </div>
+            <div class="img_container layerd"><img src="<?php bloginfo('template_url') ?>/images/page_epid/graph.png"></div>
+        </div>
+    </div>
+</div>
+
+<div class="data invisible">
+    <div id="classes_data">
+        <div class="ls_segment">
+            <div class="std_clss">
+                <div class="std_cls">碩士班</div>
+                <div class="std_cls">博士班</div>
+            </div>
+            <div class="bubbles">
+                <img src="<?php bloginfo('template_url') ?>/images/page_epid/bubble_right.png">
+                <img src="<?php bloginfo('template_url') ?>/images/page_epid/bubble_left.png">
+            </div>
+            <div class="required_ls">
+                <div class="ls_logo">必修<br>課程</div>
+                <div class="ls_content_container">
+                    <div class="ls_content">
+                        流行病學方法<br>
+                        進階流行病學研究設計<br>
+                        生物統計學<br>
+                        生物統計模式與資料分析<br>
+                        流行病學實證討論（碩）<br>
+                    </div>
+                </div>
+                <div class="ls_content_container">
+                    <div class="ls_content">
+                        流行病學方法<br>
+                        進階流行病學研究設計<br>
+                        生物統計方法<br>
+                        進階統計模式與資料分析<br>
+                        流行病學實證討論(博)<br>
+                        進階流行病學實證討論<br>
+                        流行病學教學實務<br>
+                    </div>
+                </div>
+            </div>
+            <div class="selective_ls">
+                <div class="ls_logo">選修<br>課程</div>
+                <div class="ls_content_container">
+                    <div class="ls_title">流行病學方法學類</div>
+                    <div class="ls_lr_content">
+                        <div class="ls_content">
+                            醫療照護研究 (中文)<br>
+                            醫療照護研究 (英文)<br>
+                            次級資料分析<br>
+                            健康調查方法學<br>
+                            系統性文獻回顧與統合分析<br>
+                        </div>
+                        <div class="ls_content align_right">
+                            臨床試驗導論&emsp;&emsp;<br>
+                            健康政策計量分析<br>
+                            空間流行病學&emsp;&emsp;<br>
+                        </div>
+                    </div>
+                    <div class="ls_hr"></div>
+                    <div class="ls_title">流行病學議題類</div>
+                    <div class="ls_lr_content">
+                        <div class="ls_content">
+                            <span class="ls_sub_title">遺傳流行病學</span>
+                            遺傳流行病學<br>
+                            遺傳資料分析<br>
+                            統計基因體與代謝體學<br><br>
+                            <span class="ls_sub_title">慢性病流行病學</span>
+                            營養流行病學<br>
+                            公共衛生營養：生命史與環境觀點<br>
+                            心血管流行病學<br>
+                            老化流行病學<br>
+                        </div>
+                        <div class="ls_content">
+                            <span class="ls_sub_title">精神與社會流行病學</span>
+                            物質使用與公共衛生<br>
+                            心理衛生流行病學<br>
+                            社會流行病學<br><br>
+                            <span class="ls_sub_title">臨床、傳染病與藥物流行病學</span>
+                            傳染病流行病學<br>
+                            藥物流行病學<br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ls_instru">
+                <div class="info_img_container"><img src="<?php bloginfo('template_url') ?>/images/page_epid/info.png"></div>
+                <div class="ls_instru_text">
+                    詳細課程資料請參看 <a href="#">公衛所學期課表</a><br>
+                    —<br>
+                    台灣聯大（陽清交央）相關系所選修課程：視同校內選修不受校外修課學分比例限制<br>
+                    —<br>
+                    跨領域選修課程<br>
+                    <br>
+                    由論文或課程指導教授依學生背景及研究方向輔導選修<br>
+                    請參考公衛所政策與法律，生物統計，國際衛生學程等領域課程<br>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="teachers_data"></div>
+    <div id="thoughts_data">
+        <div class="tgh_segment">
+            <div class="tgh_title">蕭逸凡【流行病學領域碩士班畢業】</div>
+            <br>
+            <div class="tgh_passage">
+                我來自於中國醫藥大學公共衛生學系，大學畢業後因為自己對著醫學與公共衛生的發展抱持著好奇的心態，所以於大學畢業前夕便決定報考研究所，很幸運地來到陽明公衛所。到陽明報到的第一天，令我印象最深刻的是每位學生的背景，這項特色將重新定義我對公共衛生的認識，這也是陽明公衛所有別於一般研究所的性質。大學以來我一直環繞在公共衛生的領域，但不諱言的和大家分享，直到大學畢業前才對於公共衛生有基礎上的認識，認識甚麼是公共衛生、流行病學、生物統計、衛生政策等，因為公共衛生要了解的知識太多除此之外還需要有實務上的經歷才能慢慢理解公共衛生的邏輯概念。來到陽明公衛所，因為這裡的學生來自社會上四面八方的人才，所以在上課的時候常常能聽到各種不同的角度來看待同一件事情。縱然用不同的角度來討論一件事情是學習上的好處，但要怎麼融合成各位學生能理解並且認同的知識，這就需要仰賴陽明公衛所的每一位老師。
+                陽明公衛所的第二項特色即是師資。公共衛生最初的目的便是將專業的知識傳達給公眾理解，但是從專業到非專業的過程中需要一道橋梁，來連接中間的鴻溝，但是這段鴻溝的管道會有很多種形式出現，所以在國外公共衛生的教學網羅了各專業領域的師資，透過他們的專業，重新定義公共衛生的概念，再次傳達給學生了解。陽明公衛所同樣也看到相似的情境，因此在師資的選擇上不單單只針對醫學、公共衛生，更有法律、統計、運動等等專業的師資來傳達公共衛生。同時在課程的開設上，也不只針對公共衛生的知識學習，更針對不同的角度來審視公共衛生在不同領域中的實務應用。這裡的老師同樣地也受到專業且多元的訓練，所以在各種課堂的討論中，除了學生們的熱烈回應外，老師們也同樣能提出相對應的回答來統合各界的認知重新傳達公共衛生。
+                我在陽明公衛所碩士生活的兩年，回首過去這段日子，論文撰寫的過程真的很辛苦但努力過後的興奮絕對遠大於之前辛苦的過程，雖然過程艱辛但陽明公衛所就像個大家庭，上至所長、老師下至學、弟妹，大家和樂融融照應彼此。過年過節時分，這裡的研究生依然持續作業，但總不忘過節的氣氛，老師們也常常出人意表的傳達過節氛圍，這段過程將會是我整個學習旅程中最棒的回憶，若問我能有機會重新選擇繼續進修的地方，我相信我依然還是會選擇陽明公共衛生研究所！
+            </div>
+            <div class="tgh_title margin_top">
+                近五年流病組碩士班校友流向
+            </div>
+            <div class="img_container layered"><img src="<?php bloginfo('template_url') ?>/images/page_epid/graph.png"></div>
+        </div>
+    </div>
+</div>
+
+<div class="deco_waves" id="footer_wave">
+    <img class="wave" src="<?php bloginfo('template_url') ?>/images/icon/footer_wave.svg">
+</div>
+<div class="block-title" id="back_to_top" onclick="topFunction()">
+    <img class="icon" src="<?php bloginfo('template_url') ?>/images/icon/back_to_top.svg">
+    <span class="en-title">back to top</span>
+</div>
+<div class=""></div>
+
+<script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/epid.js"></script>
+<?php get_footer(); ?>
