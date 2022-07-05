be8557f4bab65b0b165637fe670d840a98a2506e
YenPeiChen07 <jenny35006@gmail.com>
2022-07-05 16:05:20 +0800

    add some security code

--- functions.php
+++ functions.php
@@ -9,12 +9,63 @@
 ?>
 
 <?php
-remove_action('wp_head', 'wp_generator');
-header('Strict-Transport-Security:max-age=31536000; includeSubdomains; preload');
-header('X-Content-Type-Options: nosniff');
-@ini_set('session.cookie_httponly', true);
-@ini_set('session.cookie_secure', true);
-@ini_set('session.use_only_cookies', true);
+// Disable wp-embed
+function disable_embed_feature(){
+    wp_deregister_script( 'wp-embed' );
+}
+add_action('wp_footer', 'disable_embed_feature');
+
+// Disable wp-emoji
+function disable_emoji_feature()
+{
+    // Prevent Emoji from loading on the front-end
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('wp_print_styles', 'print_emoji_styles');
+
+    // Remove from admin area also
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+
+    // Remove from RSS feeds also
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+
+    // Remove from Embeds
+    remove_filter('embed_head', 'print_emoji_detection_script');
+
+    // Remove from emails
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+
+    // Disable from TinyMCE editor. Currently disabled in block editor by default
+    add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
+
+    /** Finally, prevent character conversion too
+     ** without this, emojis still work
+    ** if it is available on the user's device
+    */
+
+    add_filter('option_use_smilies', '__return_false');
+}
+
+// Disables emojis in WYSIWYG editor
+function disable_emojis_tinymce($plugins)
+{
+    if (is_array($plugins)) {
+        $plugins = array_diff($plugins, array('wpemoji'));
+    }
+    return $plugins;
+}
+add_action('init', 'disable_emoji_feature');
+
+?>
+
+<?php
+  remove_action('wp_head', 'wp_generator');
+  header('Strict-Transport-Security:max-age=31536000; includeSubdomains; preload');
+  header('X-Content-Type-Options: nosniff');
+  @ini_set('session.cookie_httponly', true);
+  @ini_set('session.cookie_secure', true);
+  @ini_set('session.use_only_cookies', true);
 ?>
 
 <?php
