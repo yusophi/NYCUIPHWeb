ec12919c126bcfdf40ea6def757b6f54a6cb2dc9
YenPeiChen07 <jenny35006@gmail.com>
2022-07-06 19:41:36 +0800

    prevent path traversal

--- functions.php
+++ functions.php
@@ -60,6 +60,30 @@ add_action('init', 'disable_emoji_feature');
 ?>
 
 <?php
+// Require Authentication for All Requests (REST API)
+add_filter( 'rest_authentication_errors', function( $result ) {
+    // If a previous authentication check was applied,
+    // pass that result along without modification.
+    if ( true === $result || is_wp_error( $result ) ) {
+        return $result;
+    }
+ 
+    // No authentication has been performed yet.
+    // Return an error if user is not logged in.
+    if ( ! is_user_logged_in() ) {
+        return new WP_Error(
+            'rest_not_logged_in',
+            __( 'You are not currently logged in.' ),
+            array( 'status' => 401 )
+        );
+    }
+    // Our custom authentication check should have no effect
+    // on logged-in requests
+    return $result;
+});
+?>
+
+<?php
   remove_action('wp_head', 'wp_generator');
   header('Strict-Transport-Security:max-age=31536000; includeSubdomains; preload');
   header('X-Content-Type-Options: nosniff');
