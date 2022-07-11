8e17775b713e719dfb4feac62229b2fbf9d74946
YenPeiChen07 <jenny35006@gmail.com>
2022-07-11 09:29:10 +0800

    rename with csrf_token

--- header.php
+++ header.php
@@ -195,7 +195,7 @@
                     <img id="search_svg" alt="放大鏡" src="<?php bloginfo('template_url')?>/images/header/site_search/search.svg">
                     <input type="text" value="" name="s" class="site_search_input" id="site_search_keyword" placeholder="全站搜尋"/>
                     <input type="submit" id="searchsubmit"/>
-                    <input name="csrf_token " type="hidden" value="<?php echo wp_create_nonce('test-nonce1');?>"/>
+                    <input name="csrf_token" type="hidden" value="<?php echo wp_create_nonce('test-nonce1');?>"/>
                     <?php //wp_nonce_field( 'get_site_search', 'site_search_nonce' ); ?>
                     <button id="yellow_search_btn" type="button">
                         <img alt="搜尋按鈕" src="<?php bloginfo('template_url')?>/images/header/site_search/yellow_arrow.svg">
