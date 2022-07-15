87c1be8b5c497fb4dc0575081b52cb40b4217a59
YenPeiChen07 <jenny35006@gmail.com>
2022-07-15 10:19:52 +0800

    adjust rwd in page/member

--- page-templates/template-members.php
+++ page-templates/template-members.php
@@ -18,14 +18,14 @@
     <?php 
         if($locale == "zh_TW"){
             $areas_categories = get_categories(array(
-                'parent' => 21,
+                'parent' => 33, /*21*/
                 'orderby' => 'slug',
                 'hide_empty' => false,
                 'order'   => 'ASC'
             ) );
 
             $prof_categories = get_categories(array(
-                    'parent' => 25,
+                    'parent' => 27, /*25*/
                     'orderby' => 'slug',
                     'hide_empty' => false,
                     'order'   => 'ASC'
