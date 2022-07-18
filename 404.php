4fffdaeba6e36f0b393a41346f7fce8e5e0bce23
Milo <abber.kuo.2001@gmail.com>
2022-07-19 00:31:24 +0800

    awd-links

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+    <div class="result_content">
+        <div class="article">
+            <article class="article-content">
+                <h1>404 找不到網頁</h1>
+            <p>抱歉，找不到您所要的頁面，或許已經移除、暫時關閉或發生錯誤。</p>
+            <p>您可經由<a href="<?php bloginfo('url');?>" title="回到首頁">回到首頁</a></p>
+            <?php get_search_form(); ?>
+            </article>
+        </div>
+        <?php get_template_part( 'template-parts/backtoTOP');?>    
+    </div>
+<?php get_footer(); ?>
---
+++ search.php
@@ -0,0 +1,34 @@
+<?php 
+    //$nonce = $_POST['wpnonce'];
+    if ( ! isset( $_POST['csrf_token']) || ! wp_verify_nonce($_POST['csrf_token '], 'test-nonce')):?>
+    <?php echo  'Sorry, your nonce did not verify.';
+        exit;
+    ?>
+<?php else : ?>
+    <?php get_header(); ?>
+    <div class="result_content">
+        <?php if (is_search()):?>
+            <div class="article">
+                <p id="search_keyword"> <?php echo '搜尋結果: ' . esc_html( $_POST['s'] ); ?> </p>
+                <?php if ( have_posts() ) : ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
+                        <article class="article-content">
+                            <h1 class="article-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                            <?php if('post' == get_post_type()){
+                                the_field('excerpt'); echo "...";
+                            }?>
+                            <div class="clearfix"></div>
+                        </article>
+                    <?php endwhile; ?>
+                <?php else : ?>
+                    <article class="article-content">
+                        <p>很抱歉，找不到你所搜尋的文章，你可以試著用其他關鍵字再次搜尋。</p>
+                        <?php //get_search_form(); ?>
+                    </article>
+                <?php endif; ?>
+            </div>
+        <?php endif; ?>
+        <?php get_template_part( 'template-parts/backtoTOP');?>    
+    </div>
+    <?php get_footer(); ?>
+<?php endif; ?>
