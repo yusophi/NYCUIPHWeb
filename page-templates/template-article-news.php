597dba0951a12369358e2b95599e0ff1e4301e3f
Milo <abber.kuo.2001@gmail.com>
2022-07-22 18:09:24 +0800

    手機AWD字形調整

--- page-templates/template-article-news.php
+++ page-templates/template-article-news.php
@@ -103,30 +103,28 @@
                     $counter = $counter + 1;
                     setup_postdata($post);
                 ?>
-                    <div class="article-content whiteText num-<?php echo $counter ?>">
+                    <div class="article-content num-<?php echo $counter ?>">
                         <div class="post_counter <?php echo $counter ?>"><?php echo "0" . $counter . "." ?>&nbsp;&nbsp;</div>
-                        <!--<img class="thumbnail_icon" src="<?php //bloginfo('template_url') 
-                                                                ?>/images/page_news/icon-white-newspaper.svg">
-                        <img class="thumbnail_icon_hover" src="<?php //bloginfo('template_url') 
-                                                                ?>/images/page_news/icon-white-news-more.svg">
-                        -->
-                        <div class="post_icon tmp_arti_news">
-                            <img src="<?php bloginfo('template_url') ?>/images/page_news/icon-white-newspaper.svg">
+                        <!--<img class="thumbnail_icon" src="<? php // bloginfo('template_url') 
+                                                                ?>/images/icon/icon-newspaper-new.svg">
+                                    <img class="thumbnail_icon_hover" src="<? php // bloginfo('template_url') 
+                                                                            ?>/images/icon/icon-news-more.svg">-->
+                        <div class="post_icon">
+                            <img src="<?php bloginfo('template_url') ?>/images/icon/icon-newspaper.svg">
                             <div>
                                 <p class="post_icon_hover_dots"></p>
                                 <p class="post_icon_hover_dots"></p>
                                 <p class="post_icon_hover_dots"></p>
                             </div>
                         </div>
+
                         <div class="border-anim">
                             <div class="inner-box"></div>
                         </div>
                         <div class="article-meta">
-                            <img class="icon-clock" src="<?php bloginfo('template_url') ?>/images/template-singlepost-icon/icon-whiteclock.svg">
-                            <div class="post_time"><?php the_time('Y.m.j'); ?></div>
+                            <img class="icon-clock" src="<?php bloginfo('template_url') ?>/images/icon/icon-clock.svg">
+                            <span class="post_time"><?php the_time('Y.m.j'); ?></span>
                         </div>
-                        <!--<div class="post_category"><?php //the_category(''); 
-                                                        ?></div>-->
                         <div class="post_tags">
                             <div class="post_category"><?php the_field('news_item'); //the_category(''); 
                                                         ?></div>
@@ -140,7 +138,9 @@
                                 </ul>
                             <?php endif; ?>
                         </div>
-
+                        <!--div class="article-title"><a href="<?php //the_permalink(); 
+                                                                ?>"><?php //the_title(); 
+                                                                                    ?></a></div>-->
                         <div class="article-passage">
                             <div class="article-excerpt_bottom_line"></div>
                             <div class="article-title">
@@ -150,7 +150,6 @@
                             <div class="excerpt" id="<?php echo $counter ?>"> <?php the_field('excerpt'); ?><?php echo "..." ?> </div>
                             <div class="article-excerpt_bottom_line"></div>
                         </div>
-
                         <div class="clearfix"></div>
                     </div>
                 <?php endforeach;
