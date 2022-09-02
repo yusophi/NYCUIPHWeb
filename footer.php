8e9e80859260c73d30e49340bc91aa1fe80e57c8
YenPeiChen07 <jenny35006@gmail.com>
2022-09-02 10:34:25 +0800

    make footer editable

--- footer.php
+++ footer.php
@@ -1,42 +1,33 @@
-<!-- Milo: Start of footer part -->
+<?php
+/*
+ * Template Name: footer
+ */
+?>
 <div class="Footer-container">
       <div class="footer-margin">
             <div class="footer-exclam">
-                  Ensuring Safer, Longer,
-                  Healthier Lives Through
-                  Proven Strategies.
+                  <?php the_field('slogan', 2862);?>
             </div>
             <div class="footer-divider"></div>
             <div class="footer-pic"><img src="<?php bloginfo('template_url') ?>/images/icon/icon-footer-pic.svg"></div>
             <div class="footer-info">
-                  <div>國立陽明交通大學公共衛生研究所<br>
-                        Institute of Public Health, National Yang Ming Chiao Tung University
-                  </div>
-                  <div id="footer-info-d2">國立陽明交通大學醫學系醫學人文暨教育學科<br>國立陽明交通大學醫學系公共衛生學科<br>
-                        Department of Medical Humanities & Education, School of Medicine, National Yang Ming Chiao Tung University<br>
-                        Department of Public Health, School of Medicine, National Yang Ming Chiao Tung University
-                  </div>
-                  <div id="footer-info-d3">地址：台北市北投區立農街二段155號（醫學二館）<br>
-                        Address: No. 155, Sec. 2, Linong St., Beitou Dist., Taipei City 112, Taiwan (R.O.C.)<br>
-                        電話TEL︰02-28267000 #65326、65225<br>
-                        傳真FAX︰02-2822-1942<br>
-                        研究所Email︰iph@nycu.edu.tw<br>
-                        學科Email︰dph@nycu.edu.tw
-                  </div>
+                  <div id="footer-info-d2"><?php the_field('institution_info', 2862);?></div>
+                  <div id="footer-info-d3"><?php the_field('connect_info', 2862);?></div>
             </div>
             <div class="footer-statement">
                   <div id="statement_block1">
                         <a id="info_open" class="statement" href="<?php bloginfo('template_url');?>/website-opendata-announcement/">網站資訊開放宣告</a>
                         <a id="privacy" class="statement" href="<?php bloginfo('tmeplate_url');?>/privacy-and-information-security-policy/">隱私權及資訊安全政策</a>     
                         <a id="sitemap" class="statement" href="<?php bloginfo('template_url');?>/sitemap/">網站導覽</a>
-                        <img id="icon_yt" src="<?php bloginfo('template_url') ?>/images/icon/icon-youtube_link_white.svg">
-                        <img id="icon_fb" src="<?php bloginfo('template_url') ?>/images/icon/icon-FB_link_white.svg">
+                        <?php $yt_link = get_field('youtube');
+                              $fb_link = get_field('facebook'); ?> 
+                        <a href="<?php if($yt_link){echo $yt_link;}?>"><img id="icon_yt" src="<?php bloginfo('template_url') ?>/images/icon/icon-youtube_link_white.svg"></a>
+                        <a href="<?php if($fb_link){echo $fb_link;}?>"><img id="icon_fb" src="<?php bloginfo('template_url') ?>/images/icon/icon-FB_link_white.svg"></a>
                   </div>
                   <div>
                         © 國立陽明交通大學公共衛生研究所 <?php echo date('Y') ?> &nbsp;&nbsp;&nbsp;illustration by&nbsp;
                         <a class="footer-iconscout-link" href="https://iconscout.com/">iconscout</a>
                   </div>
-                  
             </div>
       </div>
 </div>
